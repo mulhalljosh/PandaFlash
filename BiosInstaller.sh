e9fab53e574fcf32612193529c1a928861e73cdc
mulhalljosh <82676213+mulhalljosh@users.noreply.github.com>
2021-05-04 07:36:37 -0700

    Update BiosInstaller.sh

--- BiosInstaller.sh
+++ BiosInstaller.sh
@@ -9,6 +9,14 @@ sudo ./amdvbflash -p 5 $1
 sudo ./amdvbflash -p 6 $1
 sudo ./amdvbflash -p 7 $1
 
+if[ $2 -eq 12]
+then
+sudo ./amdvbflash -p 8 $1
+sudo ./amdvbflash -p 9 $1
+sudo ./amdvbflash -p 10 $1
+sudo ./amdvbflash -p 11 $1
+fi
+
 reboot
 
 exit 0

d0d31387311c6c3d7481d722b59f56b49b57cdbc
mulhalljosh <82676213+mulhalljosh@users.noreply.github.com>
2021-05-04 07:53:37 -0700

    Update BiosInstaller.sh

--- BiosInstaller.sh
+++ BiosInstaller.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-sudo ./amdvbflash -p 0 $1
-sudo ./amdvbflash -p 1 $1
-sudo ./amdvbflash -p 2 $1
-sudo ./amdvbflash -p 3 $1
-sudo ./amdvbflash -p 4 $1
-sudo ./amdvbflash -p 5 $1
-sudo ./amdvbflash -p 6 $1
-sudo ./amdvbflash -p 7 $1
+counter = 0
 
-if[ $2 -eq 12];
-then
-sudo ./amdvbflash -p 8 $1
-sudo ./amdvbflash -p 9 $1
-sudo ./amdvbflash -p 10 $1
-sudo ./amdvbflash -p 11 $1
-fi
+while[ $counter -ne $2];
+do
+  sudo ./amdvbflash -p $counter $1
+  ((counter++))
+done
 
 reboot
 
